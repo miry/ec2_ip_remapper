e3935f4a9ed12c3d69b2131ba462aff95bfc1097
Michael Nikitochkin <miry.sof@gmail.com>
2011-12-16 14:23:45 +0200

    Added ability to choose instances by tags.

--- remapper.rb
+++ remapper.rb
@@ -10,8 +10,36 @@ class Remapper
     @config = YAML.load(read_config)
     AWS.config(@config)
     @ec2 = AWS::EC2.new
-    @instances = @config['instances'].map{|instance_id| @ec2.instances[instance_id] }
+    
+    @instances = init_instance
     @public_ip = @ec2.elastic_ips[@config['elastic_ip']]
+  end
+
+  def init_instance
+    result = []
+    result = availble_instances unless @config['instances'].empty?
+    result = instances_by_tags if result.empty?
+    result
+  end
+  
+  def availble_instances
+    @config['instances'].map{|instance_id| @ec2.instances[instance_id] }.reject{|instance|(instance.status != :running) rescue true}
+  end
+  
+  def check_instances
+    @ec2.instances.each do |instance|
+      instance.status
+    end
+  end
+  
+  def instances_by_tags
+    return nil if @config['tags'].empty?
+    @config['tags'].map do |tag_name, tag_values|
+      next if tag_values.nil?
+      tag_values.map do |tag_value|
+        @ec2.instances.tagged(tag_name).tagged_values("*#{tag_value}*").map{|i| i}
+      end
+    end.flatten.compact.uniq{|instance| instance.id}.reject{|instance| (instance.status != :running) rescue true}
   end
   
   def read_config
